d682f51df0973048588943af9d1decd994ddea7a
Holoceo <holoceo@gmail.com>
2015-07-20 02:27:18 +0300

    Root checking not reliable function removed

--- app/src/main/java/com/redmadintern/mikhalevich/security/utils/RootUtil.java
+++ app/src/main/java/com/redmadintern/mikhalevich/security/utils/RootUtil.java
@@ -9,15 +9,10 @@ import java.io.InputStreamReader;
  */
 public class RootUtil {
     public static boolean isDeviceRooted() {
-        return checkRootMethod1() || checkRootMethod2() || checkRootMethod3();
+        return checkRootMethod1() || checkRootMethod2();
     }
 
     private static boolean checkRootMethod1() {
-        String buildTags = android.os.Build.TAGS;
-        return buildTags != null && buildTags.contains("test-keys");
-    }
-
-    private static boolean checkRootMethod2() {
         String[] paths = { "/system/app/Superuser.apk", "/sbin/su", "/system/bin/su", "/system/xbin/su", "/data/local/xbin/su", "/data/local/bin/su", "/system/sd/xbin/su",
                 "/system/bin/failsafe/su", "/data/local/su" };
         for (String path : paths) {
@@ -26,7 +21,7 @@ public class RootUtil {
         return false;
     }
 
-    private static boolean checkRootMethod3() {
+    private static boolean checkRootMethod2() {
         Process process = null;
         try {
             process = Runtime.getRuntime().exec(new String[] { "/system/xbin/which", "su" });
